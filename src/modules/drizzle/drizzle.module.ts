943a2215f218e267bccd33a7ad9c33a602e12e65
matheusafd <matheus.daf@outlook.com>
2025-01-25 14:28:09 -0300

    feat: add initial drizzle setup

---
+++ src/modules/drizzle/drizzle.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common'
+import {
+  DrizzleAsyncProvider,
+  drizzleProvider
+} from 'src/drizzle/drizzle.provider'
+
+@Module({
+  providers: [...drizzleProvider],
+  exports: [DrizzleAsyncProvider]
+})
+export class DrizzleModule {}
