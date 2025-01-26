c31f4cf35e7cff8ebff7ecdcd1810798df473839
matheusafd <matheus.daf@outlook.com>
2025-01-26 14:33:42 -0300

    chore: add new env variable to schema

--- env.ts
+++ env.ts
@@ -2,6 +2,7 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   NEST_API_PORT: z.coerce.number().min(1000),
+  USER_FROM_SEED_PASSWORD: z.string(),
   POSTGRES_PASSWORD: z.string(),
   POSTGRES_USER: z.string(),
   POSTGRES_DB: z.string(),
