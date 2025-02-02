30fc7eaaf8ea83f7be297c99d94a2b111fb1c5ff
matheusafd <matheus.daf@outlook.com>
2025-02-01 22:32:34 -0300

    refactor: update code due strapi integration

--- src/drizzle/seed.ts
+++ src/drizzle/seed.ts
@@ -1,14 +1,17 @@
-import { drizzle } from 'drizzle-orm/node-postgres'
 import { sql } from 'drizzle-orm'
+import { drizzle } from 'drizzle-orm/node-postgres'
+
 import { env } from 'env'
+import Stripe from 'stripe'
+
+import { RoleEnum } from '@common/enums'
+import { encryptData } from '@common/lib'
 
 import * as schema from './schema'
 
-import { RoleEnum } from 'src/common/enums'
-import { encryptData } from 'src/common/lib'
-
 async function main() {
   const db = drizzle(process.env.DATABASE_URL!)
+  const stripe = new Stripe(env.STRIPE_SECRET_KEY)
 
   const hashedPassword = await encryptData(env.USER_FROM_SEED_PASSWORD)
 
@@ -49,33 +52,44 @@ async function main() {
       throw new Error('Plan not found')
     }
 
+    const customer = await stripe.customers.create({
+      name: 'Grupo Nobre'
+    })
+
     await tx.insert(schema.companies).values({
       name: 'Grupo Nobre',
       cnpj: '26487200000104',
       cpf: '01632218046',
-      isActive: true
+      isActive: true,
+      stripeCustomerId: customer.id
     })
 
-    const createdCompany = await tx
+    const [createdCompany] = await tx
       .select()
       .from(schema.companies)
       .where(sql`(${schema.companies.name}) = 'Grupo Nobre'`)
       .limit(1)
 
+    await db
+      .update(schema.companies)
+      .set({ stripeCustomerId: customer.id })
+      .where(sql`(${schema.companies.id}) = ${createdCompany.id}`)
+
     await tx.insert(schema.activeCompanyPlans).values({
-      companyId: createdCompany[0].id,
+      companyId: createdCompany.id,
       planId: plan[0].id,
       startDate: new Date(),
       endDate: new Date(),
       isActive: true,
-      remainingDaysWithActivePlan: 30
+      remainingDaysWithActivePlan: 30,
+      stripeSubscriptionId: ''
     })
 
     await tx.insert(schema.users).values({
       name: 'user-super-admin',
       email: 'email@trial.com',
       password: hashedPassword,
-      companyId: createdCompany[0].id,
+      companyId: createdCompany.id,
       roleId: superAdminRole[0].id,
       birthday: null,
       genre: 'MALE'
